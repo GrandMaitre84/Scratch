618f8273159360dfda3bf31e07f55731825c2188
GrandMaitre84 <le.jdc.officiel.84@gmail.com>
2025-06-23 11:51:16 +0000

    Fullscreen for mobile

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Scratch & Badges",
+  "short_name": "Scratch",
+  "start_url": "./",
+  "scope": "./",
+  "display": "standalone",
+  "orientation": "portrait",
+  "background_color": "#FFD1DC",
+  "theme_color": "#28a745",
+  "icons": [
+    {
+      "src": "images/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "images/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
